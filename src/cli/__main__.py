7f871afe3930c45be210addab6e939ac65ab44ee
PhilippSchuette <p.schuette@online.de>
2023-02-20 16:28:23 +0100

    add cli entry point dummy.

---
+++ src/cli/__main__.py
@@ -0,0 +1,14 @@
+"""
+This module provides the main CLI entry point of the PyZeta project through the
+function `mainPyZeta`. At the moment it does nothing useful (yet).
+
+Authors:\n
+- Philipp Schuette\n
+"""
+
+
+def mainPyZEAL() -> None:
+    """
+    Main entry point for the CLI of the PyZeta project.
+    """
+    print("Welcome to the PyZeta project!")
