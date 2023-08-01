460ad0d4e86018bc27b6969e14e2faf6fce6f0b5
PhilippSchuette <p.schuette@online.de>
2023-08-01 14:19:12 +0200

    add Selberg zeta tests. weighted zeta WIP.

--- pyzeta/core/pyzeta_types/integral_arguments.py
+++ pyzeta/core/pyzeta_types/integral_arguments.py
@@ -9,12 +9,6 @@ from typing import TypedDict, Union
 
 from typing_extensions import TypeAlias
 
-from pyzeta.core.dynamics.function_systems.map_system import (
-    HyperbolicMapSystem,
-)
-from pyzeta.core.dynamics.function_systems.moebius_system import (
-    MoebiusMapSystem,
-)
 from pyzeta.core.pyzeta_types.general import tVec
 
 
--- pyzeta/tests/zetas/test_selberg.py
+++ pyzeta/tests/zetas/test_selberg.py
@@ -11,7 +11,10 @@ from pyzeal.pyzeal_types.estimator_types import EstimatorTypes
 from pyzeal.rootfinders.rootfinder import RootFinder
 
 from pyzeta.core.pyzeta_types.function_systems import FunctionSystemType
-from pyzeta.core.pyzeta_types.system_arguments import HyperbolicCylinderArgs
+from pyzeta.core.pyzeta_types.system_arguments import (
+    FunnelTorusArgs,
+    HyperbolicCylinderArgs,
+)
 from pyzeta.core.zetas.selberg_zeta import SelbergZeta
 from pyzeta.framework.initialization.init_modes import InitModes
 from pyzeta.framework.initialization.initialization_handler import (
@@ -53,3 +56,38 @@ def testCylinderResonances() -> None:
     assert np.allclose(expectedResonances, actualResonances, atol=1e-3)
 
     assert all(order == 2 for order in finder.orders)
+
+
+def testTorusResonances() -> None:
+    "Test Selberg zeta by calculating resonances on the funneled torus."
+    initArgs: FunnelTorusArgs = {
+        "outerLen": 10.0,
+        "innerLen": 10.0,
+        "angle": np.pi / 2.0,
+    }
+    zeta = SelbergZeta(
+        functionSystem=FunctionSystemType.FUNNEL_TORUS,
+        systemInitArgs=initArgs,
+    )
+
+    nMax = 5
+    finder = RootFinder(
+        f=lambda s: zeta(s, nMax=nMax),
+        algorithmType=AlgorithmTypes.SIMPLE_ARGUMENT,
+        estimatorType=EstimatorTypes.SUMMATION_ESTIMATOR,
+    )
+
+    expectedResonance = 0.11525
+    # check if zeta function becomes small on theoretically expected first zero
+    zetaEval = zeta(expectedResonance, nMax=2 * nMax)
+    assert np.allclose(zetaEval, 0, atol=1e-3)
+
+    # start searching for roots of the zeta function
+    finder.calculateRoots(
+        reRan=(0.11, 0.12), imRan=(-1e-5, 1e-5), precision=(4, 3)
+    )
+    actualResonances = finder.roots
+    assert len(actualResonances) == 1
+    assert np.allclose(expectedResonance, actualResonances, atol=1e-3)
+
+    assert all(order == 1 for order in finder.orders)
